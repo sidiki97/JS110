935fc7f8fef5e3e5d739fd0ffc1cbb4752ace221
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-08-12 18:18:39 +0000

    palindrome

--- lesson_2/pratice.js
+++ lesson_2/pratice.js
@@ -41,23 +41,69 @@ Compare last num to 0-5 indices (slice) in array with indexOf or include method
 
 */
 
-const readline = require('readline-sync');
+// const readline = require('readline-sync');
 
-let nums = [];
+// // Searching 101
 
-for (let i = 1; i < 7; i += 1) {
-  if (i === 6) {
-    let last = readline.question('Enter the last number: ');
-    nums.push(last);
-  } else {
-    let num = readline.question(`Enter the ${i}th number: `);
-    nums.push(num);
+// let nums = [];
+
+// for (let i = 1; i < 7; i += 1) {
+//   if (i === 6) {
+//     let last = readline.question('Enter the last number: ');
+//     nums.push(last);
+//   } else {
+//     let num = readline.question(`Enter the ${i}th number: `);
+//     nums.push(num);
+//   }
+// }
+
+// let end = nums[nums.length - 1];
+// if (nums.slice(0, 5).includes(end)) {
+//   console.log(`The number ${end} appears in ${nums.slice(0,5)}`);
+// } else {
+//   console.log(`The number ${end} does not appear in ${nums.slice(0,5)}`);
+// }
+
+
+// Palindromic Strings (Part 1)
+
+/*
+
+INPUT: string 
+OUTPUT: Boolean
+
+Compare left characters to right character
+
+If string empty or one char, return false
+
+- Find the midpt of the string
+- Iterate string till midpt
+- Compare character with character from other side
+  - Ex. i = 0;
+  - Char other side string.length - 1 - i
+  - If not equal, return false
+
+
+return true
+
+*/
+
+function palindrome(string) {
+  if (string.length <= 1) {
+    return false;
   }
+
+  let midpt = Math.floor((string.length - 1) / 2);
+  
+  for (let i = 0; i < midpt; i += 1) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-let end = nums[nums.length - 1];
-if (nums.slice(0, 5).includes(end)) {
-  console.log(`The number ${end} appears in ${nums.slice(0,5)}`);
-} else {
-  console.log(`The number ${end} does not appear in ${nums.slice(0,5)}`);
-}
+console.log(palindrome('moms'));
+console.log(palindrome("mom"));
+console.log(palindrome('Madam'));
