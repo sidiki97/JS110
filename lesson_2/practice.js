189ee76c968eaf4d5db650bf396c269629f961ab
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-08-23 21:02:05 +0000

    problems

--- lesson_2/practice.js
+++ lesson_2/practice.js
@@ -137,3 +137,38 @@ let arri = [
 //     }
 //   ) }
 //   ))
+
+const ARR = [
+  { one: '1', two: 2 },
+  [ { four: 5, three: 6 }, 'three' ],
+  'three',
+  { '2': 'two', '3': 'three' }
+]
+
+let todoLists = [
+  {
+    id: 1,
+    listName: 'Groceries',
+    todos: [
+      { id: 1, name: 'Bread', completed: false },
+      { id: 2, name: 'Milk', completed: false },
+      { id: 3, name: 'Apple Juice', completed: false }
+    ]
+  }
+];
+todoLists[0]['todos'][2]['name'] = 'Orange Juice';
+
+function evenValues(array) {
+  let evens = [];
+
+  array.forEach(value => {
+    if (value % 2 === 0) {
+      evens.push(value);
+    }
+    array.shift();
+  });
+
+  return evens;
+}
+debugger;
+console.log(evenValues([1, 3, 4, 2, 4, 6, 5, 7, 9, 10, 12]));
