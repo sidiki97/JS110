2676e44f0fe8664d05e0d8835b0efdafa84ba46d
sidiki97 <62025524+sidiki97@users.noreply.github.com>
2023-08-23 23:05:01 +0000

    finished

--- lesson_3/tictactoe.js
+++ lesson_3/tictactoe.js
@@ -12,6 +12,8 @@ function prompt (message) {
 function displayBoard(board) {
   console.clear();
 
+  console.log(`You are ${HUMAN_MARKER}. Computer is ${COMPUTER_MARKER}`);
+
   console.log('');
   console.log('     |     |');
   console.log(`  ${board['1']}  |  ${board['2']}  |  ${board['3']}`);
@@ -78,16 +80,66 @@ function boardFull(board) {
 // displayBoard(board);
 
 while (true) {
-  playerChoosesSquare(board);
-  computerChoosesSquare(board);
+  let board = initializeBoard();
+
+  while (true) {
+    displayBoard(board);
+
+    playerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+
+    computerChoosesSquare(board);
+    if (someoneWon(board) || boardFull(board)) break;
+  }
+
   displayBoard(board);
+
   if (someoneWon(board)) {
     prompt(`${detectWinner(board)} won!`);
   } else {
     prompt("It's a tie!");
   }
+
+  prompt('Play again? (y or n)');
+  let answer = readline.question().toLowerCase()[0];
+  if (answer !== 'y') break;
 }
+
+prompt('Thanks for playing Tic Tac Toe!');
+
+
+
+
 
 function someoneWon(board) {
   return !!detectWinner(board);
+}
+
+
+function detectWinner(board) {
+  let winningLines = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9], // rows
+    [1, 4, 7], [2, 5, 8], [3, 6, 9], // columns
+    [1, 5, 9], [3, 5, 7]             // diagonals
+  ];
+
+  for (let line = 0; line < winningLines.length; line++) {
+    let [ sq1, sq2, sq3 ] = winningLines[line];
+
+    if (
+        board[sq1] === HUMAN_MARKER &&
+        board[sq2] === HUMAN_MARKER &&
+        board[sq3] === HUMAN_MARKER
+    ) {
+      return 'Player';
+    } else if (
+        board[sq1] === COMPUTER_MARKER &&
+        board[sq2] === COMPUTER_MARKER &&
+        board[sq3] === COMPUTER_MARKER
+    ) {
+      return 'Computer';
+    }
+  }
+
+  return null;
 }
